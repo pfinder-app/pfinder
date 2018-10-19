8caf326f15ab596d5b682d4c5faaeeaec14d08d4
Emily Wangler <em.wangler@gmail.com>
2018-10-19 17:22:48 +0200

    add logo and play around

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,41 +1,66 @@
 <template>
-  <v-ons-page>
-    <v-ons-toolbar>
-      <div class="center">Pfinder Home</div>
-    </v-ons-toolbar>
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="center">Pfinder</div>
+        </v-ons-toolbar>
 
-    <v-ons-card>
-      <div class="title">
-        Awesome App
-      </div>
-      <div class="content">
-        This is an example page<br><br>
-        <v-ons-button @click="showAbout">
-          Show About
-        </v-ons-button>
-      </div>
-    </v-ons-card>
-    <br>
-    <br>
-    <br>
-    Eingeloggt als <b>{{$store.state.email}}</b>. <a @click="logout">Abmelden</a>
-  </v-ons-page>
+        <v-ons-card>
+            <div class="title">
+                Wilkommen bei Pfinder
+            </div>
+            <div class="content">
+                <ons-row style="margin-top: 10px">
+                    <ons-col><v-ons-button modifier="large">Pfadis finden</v-ons-button></ons-col>
+                </ons-row>
+                <ons-row style="margin-top: 10px">
+                    <ons-col width="50%">
+                        <v-ons-button class="menu-button-half">Freunde</v-ons-button>
+                    </ons-col>
+                    <ons-col>
+                        <v-ons-button class="menu-button-half">Wichtig</v-ons-button>
+                    </ons-col>
+                </ons-row>
+                <ons-row style="margin-top: 10px">
+                    <ons-col width="50%">
+                        <v-ons-button class="menu-button-half">Treffpunkt</v-ons-button>
+                    </ons-col>
+                    <ons-col>
+                        <v-ons-button class="menu-button-half">Hä?</v-ons-button>
+                    </ons-col>
+                </ons-row>
+                <ons-row style="margin-top: 10px">
+                    <ons-col>
+                        <v-ons-button modifier="large">
+                            <ons-icon icon="qrcode"></ons-icon> Connect</v-ons-button>
+                    </ons-col>
+                </ons-row>
+            </div>
+        </v-ons-card>
+        Eingeloggt als <b>{{$store.state.email}}</b>. <a @click="logout">Abmelden</a>
+    </v-ons-page>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import Login from '@/views/Login.vue';
-import About from '@/views/About.vue';
+    import { Component, Vue } from 'vue-property-decorator';
+    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import Login from '@/views/Login.vue';
+    import About from '@/views/About.vue';
 
-@Component({})
-export default class Home extends Vue {
-  public logout() {
-    this.$store.commit('setEmail', '');
-    this.$emit('replace-page', Login);
-  }
-  public showAbout() {
-    this.$emit('push-page', About);
-  }
-}
+    @Component({})
+    export default class Home extends Vue {
+        public logout() {
+            this.$store.commit('setEmail', '');
+            this.$emit('replace-page', Login);
+        }
+
+        public showAbout() {
+            this.$emit('push-page', About);
+        }
+    }
 </script>
+
+<style lang="scss">
+    .menu-button-half {
+        width: 98%;
+    }
+</style>
