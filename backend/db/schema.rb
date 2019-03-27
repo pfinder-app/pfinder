4aebf1c000bbe8c58fa50e9778099a448672ada0
Simon Huber <simon.huber@renuo.ch>
2019-03-27 21:34:54 +0100

    Fix docker errors

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_929_184_055) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2018_09_29_184055) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'scoutname'
-    t.date 'birthdate'
-    t.string 'scoutgroup'
-    t.integer 'canton'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "scoutname"
+    t.date "birthdate"
+    t.string "scoutgroup"
+    t.integer "canton"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
