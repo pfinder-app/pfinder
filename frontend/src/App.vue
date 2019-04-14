5e32558d6752149fdf2439b6db2f214d7eef32aa
Demian Holderegger <demian.holderegger@gmail.com>
2019-04-14 16:40:24 +0200

    add app-menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
     <ion-app>
-      <ion-vue-router />
+      <app-menu></app-menu>
+      <ion-vue-router main></ion-vue-router>
     </ion-app>
+    <ion-menu-controller></ion-menu-controller>
   </div>
 </template>
 
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import AppMenu from "@/components/AppMenu.vue";
+
+  @Component({
+    components: {
+      AppMenu
+    }
+  })
+  export default class App extends Vue {}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ frontend/src/components/AppMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-menu side="start">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Pfinder</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <router-link to="/">
+          <ion-item @click="close()">
+            <ion-icon name="home" slot="start"></ion-icon>
+            <ion-label>
+              Home
+            </ion-label>
+          </ion-item>
+        </router-link>
+        <router-link to="/about">
+          <ion-item @click="close()">
+            <ion-icon name="information-circle-outline" slot="start"></ion-icon>
+            <ion-label>
+              About
+            </ion-label>
+          </ion-item>
+        </router-link>
+      </ion-list>
+    </ion-content>
+  </ion-menu>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AppMenu extends Vue {
+  public close() {
+    // this is kind of hacky, but i couldn't find a better way
+    document.querySelector('ion-menu-controller').close('start');
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
