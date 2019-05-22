9b6c66d9e6dff359ed7463e025d83094761bf043
Demian Holderegger <demian.holderegger@gmail.com>
2019-05-22 21:17:09 +0200

    add register form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
     <ion-app>
-      <app-menu></app-menu>
-      <ion-vue-router main></ion-vue-router>
+      <template v-if="isLoggedIn">
+        <app-menu></app-menu>
+        <ion-vue-router main></ion-vue-router>
+      </template>
+      <template v-else>
+        register
+        <register></register>
+      </template>
     </ion-app>
     <ion-menu-controller></ion-menu-controller>
   </div>
@@ -13,13 +19,20 @@
   import AppMenu from "@/components/AppMenu.vue";
   import {IUser} from "@/interfaces/IUser";
   import axios from 'axios';
+  import Register from "@/views/Register.vue";
 
   @Component({
     components: {
+      Register,
       AppMenu
     }
   })
   export default class App extends Vue {
+
+    get isLoggedIn() {
+      return this.$store.state.App.isLoggedIn;
+    }
+
     mounted() {
       axios.get('/api/users/1').then((response) => {
         this.$store.commit('SET_USER', response.data.data)
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="ion-page">
+    <ion-content class="ion-padding">
+      <div style="text-align: center;">
+        <h1>Wilkommen bei Pfinder</h1>
+        <img alt="Pfinder Logo" src="../assets/logo.png" width="100">
+        <div>
+          <input type="text" v-model="scoutname" placeholder="Pfadiname" class="register-input">
+        </div>
+        <div>
+          <input type="email" v-model="email" placeholder="E-Mail" class="register-input">
+        </div>
+        <div>
+          <ion-button @click="register()" shape="round" size="large" :disabled="!isValid">Mitmachen</ion-button>
+        </div>
+      </div>
+    </ion-content>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import axios from 'axios';
+
+  @Component({})
+  export default class Register extends Vue {
+
+    private scoutname: string = '';
+    private email: string = '';
+
+    get isValid() {
+      // TODO: better check email address
+      return (this.scoutname.length > 0 && this.email.length > 0);
+    }
+
+    mounted() {
+    }
+
+    public register() {
+      if (this.isValid) {
+        // TODO: move to after registration when it works
+
+        let data = {
+          scoutname: this.scoutname,
+          email: this.email,
+        };
+        axios.post('/api/me', { data: data }).then((response) => {
+          this.$store.commit('SET_LOGGEDIN', true);
+        })
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  .register-input {
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #7f4b19;
+    margin-bottom: 10px;
+    width: 80%;
+  }
+</style>
+
