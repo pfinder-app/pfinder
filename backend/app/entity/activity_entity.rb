6b319fc810a1edfffaa32df40048429d3a28480e
Emily Wangler <em.wangler@gmail.com>
2019-06-13 15:51:17 +0200

    fix more tests

--- backend/app/entity/activity_entity.rb
+++ backend/app/entity/activity_entity.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ActivityEntity < Grape::Entity
-  expose :id, :place, :begins_at, :duration, :description, :creator_id, :max_participants
+  expose :id, :place, :title, :begins_at, :duration, :description, :creator_id, :max_participants
 end
--- backend/spec/models/activity_spec.rb
+++ backend/spec/models/activity_spec.rb
@@ -5,15 +5,16 @@ require 'rails_helper'
 RSpec.describe Activity, type: :model do
   subject { Activity.find(activity.id) }
 
-  let(:activity) { create(:activity) }
+  let(:user) { create(:user) }
+  let(:activity) { create(:activity, creator: user) }
 
-  context 'validations' do
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:place) }
-    it { should validate_presence_of(:begins_at) }
+  context 'with validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:place) }
+    it { is_expected.to validate_presence_of(:begins_at) }
   end
 
-  context 'relations' do
-    it { should belong_to(:creator) }
+  context 'with relations' do
+    it { is_expected.to belong_to(:creator) }
   end
 end
